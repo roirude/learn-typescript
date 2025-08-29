611939be1e4fcca373e6875735de167c26175e0a
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-29 15:18:44 +0100

    Run code and display error

--- pizza-app/index.js
+++ pizza-app/index.js
@@ -26,17 +26,21 @@ function placeOrder(pizzaName) {
     return newOrder;
 }
 
-/**
- * Challenge: write another utility function, completeOrder, that takes an orderId as a parameter
- * finds the correct order in the orderQueue, and marks its status as "completed". For good measure,
- * return the found order from the function.
- *
- * Note: you'll need to ensure that we're adding IDs to our orders when we create new orders. You can use a global `nextOrderId` variable and increment it every time a new order is created to simulate real IDs being managed for us by a database.
- */
-
 function completeOrder(orderId) {
     const order = orderQueue.find((orderObj) => orderObj.id === orderId);
     order.status = "completed";
 
     return order;
 }
+
+
+addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
+addNewPizza({ name: "BBQ Chicken", cost: 12 })
+addNewPizza({ name: "Spicy Sausage", cost: 11 })
+
+placeOrder("Chicken Bacon Ranch")
+completeOrder("1")
+
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
