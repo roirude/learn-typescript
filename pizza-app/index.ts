dc5aa7c1a77fb93ed0a759849710f613f55c88f4
Rudex Tede <rudexjuniortede@gmail.com>
2025-08-31 01:23:53 +0100

    add generic assToArray function

--- pizza-app/index.ts
+++ pizza-app/index.ts
@@ -49,6 +49,24 @@ function placeOrder(pizzaName: string): Order | undefined {
     return newOrder;
 }
 
+/**
+ * Challenge: add types our generic `addToArray` function. It should work
+ * for adding new pizzas to the `menu` and adding new orders to the `orderQueue`
+ */
+
+function addToArray<T>(array: T[], item: T) {
+    array.push(item);
+    return array;
+}
+
+// example usage:
+addToArray(menu, { id: pizzaId++, name: "Chicken Bacon Ranch", price: 12 });
+addToArray(orderQueue, {
+    id: nextOrderId++,
+    pizza: menu[2],
+    status: "completed",
+});
+
 function completeOrder(orderId: number): Order | undefined {
     const order = orderQueue.find((orderObj: Order) => orderObj.id === orderId);
     if (!order) {
