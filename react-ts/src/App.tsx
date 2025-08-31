f1cd579d3d9e3374e12d52b0faac43b2f883fd52
Rudex Tede <rudexjuniortede@gmail.com>
2025-09-01 00:06:08 +0100

    Typing Derived values and arrow functions

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -11,15 +11,15 @@ export default function AssemblyEndgame() {
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
   // Derived values
-  const numGuessesLeft = languages.length - 1
-  const wrongGuessCount =
-    guessedLetters.filter(letter => !currentWord.includes(letter)).length
-  const isGameWon =
-    currentWord.split("").every(letter => guessedLetters.includes(letter))
-  const isGameLost = wrongGuessCount >= numGuessesLeft
-  const isGameOver = isGameWon || isGameLost
-  const lastGuessedLetter = guessedLetters[guessedLetters.length - 1]
-  const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
+  const numGuessesLeft: number = languages.length - 1
+  const wrongGuessCount: number =
+    guessedLetters.filter((letter: string): boolean => !currentWord.includes(letter)).length
+  const isGameWon: boolean =
+    currentWord.split("").every((letter: string): boolean => guessedLetters.includes(letter))
+  const isGameLost: boolean = wrongGuessCount >= numGuessesLeft
+  const isGameOver: boolean = isGameWon || isGameLost
+  const lastGuessedLetter: string = guessedLetters[guessedLetters.length - 1]
+  const isLastGuessIncorrect: boolean | "" = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
 
   // Static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
