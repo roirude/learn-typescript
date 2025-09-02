422f6994a2814d6ab74350e3a049ade63e65d98f
Rudex Tede <rudexjuniortede@gmail.com>
2025-09-02 05:43:44 +0100

    Refactor project

---
+++ react-ts/src/components/Header.tsx
@@ -0,0 +1,26 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+        </header>
+    )
+}
+
+/*
+TYPING USING REACT.FC
+
+const Header: React.FC<props> = () => {
+    return (
+        <header>
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+        </header>
+    );
+};
+
+export default Header;
+
+*/
